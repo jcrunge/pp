36a1bedff92561e69051b23bdb650c1b270f71ae
jcrunge <juan.crunge@gmail.com>
2016-11-09 20:37:10 -0600

    Suma Pi con OpenMP

---
+++ sumapiOpenMP.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+
+void sumaPi(void);
+
+int main(int argc, char *argv[])
+{
+	int numeroDeHilos = strtol(argv[1], NULL, 10);
+	int n=0;
+	double factor;
+	double sum = 0.0, pi_approx;
+	int k;
+	#pragma omp parallel for num_threads(numeroDeHilos) \
+	 default(none) reduction(+:sum) private(k, factor) \
+	 shared(n)
+	
+	for(k= 0; k<n; k++){
+		if(k%2 == 0){
+			factor=1.0;
+		}
+
+		else{
+
+			factor=-1.0;
+		}	
+		sum += factor / (2*k+1);	
+	}
+
+	pi_approx = 4.0*sum;
+	printf("Suma %lf\n", pi_approx);
+
+	return 0;
+}
