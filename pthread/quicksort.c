8b63943c3bbb0a2a47af6b3796725324acaad6e9
jcrunge <juan.crunge@gmail.com>
2016-11-24 23:31:38 -0600

    quicksort cambios

--- pthread/quicksort.c
+++ pthread/quicksort.c
@@ -5,7 +5,7 @@
 using namespace std;
 
 pthread_mutex_t lock;
-int arr[40];
+int lista[40];
 
 struct por
 {
@@ -13,41 +13,44 @@ struct por
     int second;    
 };
 
-void * quickSort(void * par)
+void * qs(void * par)
 {
     por *mi_par = (por *)par;
     int izq = mi_par->first, der = mi_par->second;
 
     int i = izq, j = der;
     int tmp;
-    int pivote = arr[(izq + der) / 2];
-
+    int mitad = lista[(izq + der) / 2];
+    //do{
+        //while(lista[i]< mitad && i< der)i++;
+        //while(mitad < lista[j] && j > izq)j--;
+    
     /* partition */
     while (i <= j) 
     {
-        while (arr[i] < pivote)
+        while (lista[i] < mitad)
             i++;
-        while (arr[j] > pivote)
+        while (lista[j] > mitad)
             j--;
         if (i <= j) 
         {
-            tmp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = tmp;
+            tmp = lista[i];
+            lista[i] = lista[j];
+            lista[j] = tmp;
             i++;
             j--;
         }
-    }
-
+    }//while(i<=j);
+    //}
     por par1{izq,j}, par2{i, der};
     
     pthread_t* manejador_de_hilos;
     manejador_de_hilos = (pthread_t * )malloc( 2 * sizeof(pthread_t) );
 
     if (izq < j)
-        pthread_create( &manejador_de_hilos[0], NULL, quickSort, (void *)&par1 );
+        pthread_create( &manejador_de_hilos[0], NULL, qs, (void *)&par1 );
     if (i < der)
-        pthread_create( &manejador_de_hilos[1], NULL, quickSort, (void *)&par2 );        
+        pthread_create( &manejador_de_hilos[1], NULL, qs, (void *)&par2 );        
     
     pthread_join(manejador_de_hilos[0], NULL);
     pthread_join(manejador_de_hilos[1], NULL);
@@ -64,19 +67,19 @@ int main()
     
     scanf("%d", &n);
     for (int i = 0; i < n; ++i)
-          scanf("%d", &arr[i]);
+          scanf("%d", &lista[i]);
 
     por par1{0,n-1};
     pthread_t* manejador_de_hilos;
     manejador_de_hilos = (pthread_t * )malloc( 1 * sizeof(pthread_t) );
 
-    pthread_create( &manejador_de_hilos[0], NULL, quickSort, (void *)&par1 );
+    pthread_create( &manejador_de_hilos[0], NULL, qs, (void *)&par1 );
     pthread_join(manejador_de_hilos[0], NULL);
 
     free(manejador_de_hilos);
 
     for (int i = 0; i < n; ++i)
-          printf("%d ", arr[i]);
+          printf("%d ", lista[i]);
     printf("\n");    
     return 0;
 }
